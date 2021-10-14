75493db62e2bd5e6ca261a13023865c29b813b3d
VanPhuong1411 <91312257+VanPhuong1411@users.noreply.github.com>
2021-10-14 09:57:58 +0700

    Add files via upload

---
+++ TCPServerSS.java
@@ -0,0 +1,28 @@
+package ThucHanh;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServerSS {
+    ublic static void main(String[] args) {
+        ServerSocket ss;
+        try {
+            ss = new ServerSocket(6789);
+            System.out.println("SERVER song song DA~ DUOC TAO");
+            while (true) {
+                Socket s = ss.accept();
+
+                XulyLuong p = new XulyLuong(s);
+                p.start();
+
+                // ss.close();
+
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
